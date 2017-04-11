9bb5a0f05a3fb84ff1b45dd57a46f62ba73e9978
sadiyah faruk <sf2ne@virginia.edu>
2017-04-11 16:34:16 -0400

    made first .csv now to try phpmyadmin

--- dev/harvest/harvest.py
+++ dev/harvest/harvest.py
@@ -2,13 +2,24 @@
 
 import codecs
 import unicodedata
+import csv
+import numpy as np
+import re
 
-
-f = codecs.open('plant_data/Virginia.txt', 'r', encoding='utf-8')
-va_arr = []
-state = "Virginia"
+f = codecs.open('/Users/sadiyahfaruk/schoolstuff/database/dbproj/alabama.txt', 'r', encoding='utf-8')
+state_arr = []
+node = []
+state = "Alabama"
+state_acronym = "AL"
+key_num = 1
+filename = state + ".csv"
+b = open(filename, 'w')
+a = csv.writer(b)
 
 for line in f.readlines():
+    if key_num == 1:
+        key_num += 1
+        continue
     unicodedata.normalize('NFKD', line).encode('ascii', 'ignore')
     line = line.strip('\n')
     line = line.split(',')
@@ -36,6 +47,16 @@ for line in f.readlines():
         common_name = "None"
     if family == "":
         family = "None"
+    p_key = str(key_num)+state_acronym
+    regex = re.compile("[^a-zA-Z]")
+    if 'ssp.' in author or 'var.' in author:
+        continue
+    if len(author) == 1:
+        author[0] = regex.sub('', author[0])
+    if len(author) > 1:
+        for i in range(0, len(author)):
+            author[i] = regex.sub('', author[i])
+
     #
     # print()
     # print("Symbol", symbol)
@@ -45,7 +66,24 @@ for line in f.readlines():
     # print("National Common Name", common_name)
     # print("Family", family)
 
-    va_arr = [symbol, syn_symbol, genus, species, author, common_name, family, state]
+    node = [p_key, symbol, syn_symbol, genus, species, author, common_name, family, state]
+    # n = np.array(node)
+    # a.writerows(n)
+    key_num += 1
+    state_arr.append(node)
+    print(p_key)
+    # a.writerows(node)
+    # print(node)
+    # print(p_key)
+    # if key_num == 3:
+    #     break
 
-    print(va_arr)
+# print(state_arr)
+# data = np.array(state_arr)
+a.writerows(state_arr)
+b.close()
+
+
+
+
 
