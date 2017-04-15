654b15b7ad6563536311e8d02c2e59721268e096
Neha Telhan <nt7ab@virginia.edu>
2017-04-15 12:45:41 -0400

    Making progress on connecting front end to database.

--- UserInsert.php
+++ UserInsert.php
@@ -1,21 +1,30 @@
+
 <?php
 include_once("./library.php"); // To connect to the database
 $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 // Check connection
 if (mysqli_connect_errno())
   {
-    echo "Failed to connect to MySQL: " . mysqli_connect_err\
-      or();
+    echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
 // Form the SQL query (an INSERT query)
- $sql="INSERT INTO User (First_Name, Last_Name, State, Region)
- VALUES
- ('$_POST[firstname]','$_POST[lastname]')";
+$first_name = $_POST['firstname'];
+$last_name = $_POST['lastname'];
+
+echo "$first_name";
+echo "$last_name";
+
+//$sql="INSERT INTO User (First_Name, Last_Name) VALUES ('$first_name', '$last_name')";
+$sql="INSERT INTO User VALUES (50, 'Jack', 'Dawson')";
+
 
 if (!mysqli_query($con,$sql))
   {
     die('Error: ' . mysqli_error($con));
   }
-echo "1 record added"; // Output to user
+else
+  {
+  echo "1 record added"; // Output to user
+  }
 mysqli_close($con);
 ?>
