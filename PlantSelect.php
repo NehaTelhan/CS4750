2924229fe468c8a70b544cc003aeaf796a6df010
sadiyah faruk <sf2ne@virginia.edu>
2017-04-25 21:14:40 -0400

    json stuff

--- PlantSelect.php
+++ PlantSelect.php
@@ -3,6 +3,9 @@ require "dbconnect.php";
 $db = DbUtil::loginConnection();
 
 $stmt = $db->stmt_init();
+$response = array();
+$posts = array();
+$fp = fopen('/Applications/XAMPP/xamppfiles/htdocs/CS4750/results.json', 'w');
 
 if($stmt->prepare("select pid, symbol, genus, species, cname, family from Plant where cname like ?") or die(mysqli_error($db))) {
   $searchString = '%' . $_GET['searchName'] . '%';
@@ -34,6 +37,7 @@ table#t01 th {
 
 </style>
 <table id=t01 style=width:100%  border=1>
+<th><strong>PlantID</strong></th>
 <th><strong>Symbol</strong></th>
 <th><strong>Genus</strong></th>
 <th><strong>Species</strong></th>
@@ -43,14 +47,23 @@ table#t01 th {
 ;
 
  while($stmt->fetch()) {
-    echo "<tr><td>$symbol</td><td>$genus</td><td>$species</td><td>$cname</td><td>$family</td></tr>";
+    echo "<tr><td>$pid</td><td>$symbol</td><td>$genus</td><td>$species</td><td>$cname</td><td>$family</td></tr>";
+    $posts[] = array("$pid", "$symbol", "$genus", "$species", "$cname", "$family");
+    fwrite($fp, json_encode(array("$pid", "$symbol", "$genus", "$species", "$cname", "$family")));
+
   }
   echo "</table>";
 
+echo "<div style='text-align: center'><font color='white'><a href='/Applications/XAMPP/xamppfiles/htdocs/CS4750/results.json' download='plantreport.json'></font>Export</a></div>";
+
+//  $response[] = $posts;
   $stmt->close();
 }
-
 $db->close();
+
+
+//fwrite($fp, json_encode($posts));
+fclose($fp);
 
 
 ?>
