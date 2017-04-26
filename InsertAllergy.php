e54aca43f0f05009cf7f668ad70155a0f46713a4
apn2my <apn2my@labunix01.cs.virginia.edu>
2017-04-26 00:15:42 -0400

    insert UI

--- InsertAllergy.php
+++ InsertAllergy.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once("./library.php"); // To connect to the database
 $con = new mysqli($SERVER, $USERNAME, $PASSWORD, $DATABASE);
 
@@ -8,13 +7,9 @@ if (mysqli_connect_errno())
   {
     echo "Failed to connect to MySQL: " . mysqli_connect_error();
   }
-
-
 session_start();
-print_r($_SESSION);
-
+//print_r($_SESSION);
 $email = $_SESSION['Email'];
-
 $query1 = "SELECT uid,email FROM User";
 $result = mysqli_query($con, $query1);
 
@@ -25,47 +20,37 @@ while($row = mysqli_fetch_array($result)) {
 }
 
 $uid = $_SESSION['UID'];
-
-
 $allergy_list = $_POST['allergy'];
 echo "ALLERGY LIST: $allergy_list";
-
 $parsed = explode(',', $allergy_list);
-
 $cart = array();
 
 foreach($parsed as $value) {
   $sql = "SELECT pid, cname FROM Plant";
   $result = mysqli_query($con, $sql);
-  
   while ($row = mysqli_fetch_array($result)) {
     if ($row['cname'] == $value) {
       $insert_pid = $row['pid'];
-      //echo "INSERT PID: $insert_pid";
-      // echo "<br>";
-      
       array_push($cart, $insert_pid);
-
+ 
     }
   }
 }
-//print_r(array_values($cart));
 
 foreach($cart as $item) {
   echo "<br>";
   echo "$item";
   echo "<br>";
-
   $query = "INSERT INTO Allergic_To (uid, pid) VALUES ('$uid', '$item')";
-
-  if (!mysqli_query($con,$query))
-    {
-      die('Error: ' . mysqli_error($con));
-    }
-  else{
-    echo "PROBLEM!";
-  }
 }
-mysqli_close($con);
+  //Printing to User
 
+if (!mysqli_query($con,$query))
+  {
+    die('Error: ' . mysqli_error($con));
+  }
+else{
+}
+
+mysqli_close($con);
 ?>
